2a4c33ab429498650e956df2b8613dd7ffd867d9
Matthias Huber <Matthias Huber>
2015-11-17 19:15:53 +0100

    Added model rotation and zooming

---
+++ Assets/Scripts/Patient/ModelRotator.cs
@@ -0,0 +1,111 @@
+﻿// Model Rotator 
+// based on SimpleMouseRotator from unity utility
+
+using System;
+using UnityEngine;
+
+
+namespace UnityStandardAssets.Utility
+{
+    public class ModelRotator : MonoBehaviour
+    {
+        // A mouselook behaviour with constraints which operate relative to
+        // this gameobject's initial rotation.
+        // Only rotates around local X and Y.
+        // Works in local coordinates, so if this object is parented
+        // to another moving gameobject, its local constraints will
+        // operate correctly
+        // (Think: looking out the side window of a car, or a gun turret
+        // on a moving spaceship with a limited angular range)
+        // to have no constraints on an axis, set the rotationRange to 360 or greater.
+
+        public float rotationSpeed = 10;
+        public float dampingTime = 0.2f;
+        public bool autoZeroVerticalOnMobile = true;
+        public bool autoZeroHorizontalOnMobile = false;
+
+
+        private Vector3 m_TargetAngles;
+        private Vector3 m_FollowAngles;
+        private Vector3 m_FollowVelocity;
+        private Quaternion m_OriginalRotation;
+
+
+        private void Start()
+        {
+            m_OriginalRotation = transform.localRotation;
+        }
+
+
+        private void Update()
+        {
+
+            if (Input.GetMouseButton(2) || Input.GetKey(KeyCode.M)) //TODO maybe check if traget is reached (if button is released now, rotation stops)
+            {               
+                // we make initial calculations from the original local rotation
+                transform.localRotation = m_OriginalRotation;
+
+            // read input from mouse or mobile controls
+            float inputH;
+            float inputV;
+
+                inputH = -Input.GetAxis("Mouse X");
+                inputV = -Input.GetAxis("Mouse Y");
+
+                // wrap values to avoid springing quickly the wrong way from positive to negative
+                if (m_TargetAngles.y > 180)
+                {
+                    m_TargetAngles.y -= 360;
+                    m_FollowAngles.y -= 360;
+                }
+                if (m_TargetAngles.x > 180)
+                {
+                    m_TargetAngles.x -= 360;
+                    m_FollowAngles.x -= 360;
+                }
+                if (m_TargetAngles.y < -180)
+                {
+                    m_TargetAngles.y += 360;
+                    m_FollowAngles.y += 360;
+                }
+                if (m_TargetAngles.x < -180)
+                {
+                    m_TargetAngles.x += 360;
+                    m_FollowAngles.x += 360;
+                }
+
+#if MOBILE_INPUT
+            // on mobile, sometimes we want input mapped directly to tilt value,
+            // so it springs back automatically when the look input is released.
+			if (autoZeroHorizontalOnMobile) {
+				m_TargetAngles.y = Mathf.Lerp (-rotationRange.y * 0.5f, rotationRange.y * 0.5f, inputH * .5f + .5f);
+			} else {
+				m_TargetAngles.y += inputH * rotationSpeed;
+			}
+			if (autoZeroVerticalOnMobile) {
+				m_TargetAngles.x = Mathf.Lerp (-rotationRange.x * 0.5f, rotationRange.x * 0.5f, inputV * .5f + .5f);
+			} else {
+				m_TargetAngles.x += inputV * rotationSpeed;
+			}
+#else
+                // with mouse input, we have direct control with no springback required.
+                m_TargetAngles.y += inputH * rotationSpeed;
+                m_TargetAngles.x += inputV * rotationSpeed;
+#endif
+            
+            // smoothly interpolate current values to target angles
+            m_FollowAngles = Vector3.SmoothDamp(m_FollowAngles, m_TargetAngles, ref m_FollowVelocity, dampingTime);
+
+                // update the actual gameobject's rotation
+                Quaternion t =  m_OriginalRotation * Quaternion.Euler(0, m_FollowAngles.y, 0);
+                transform.localRotation = t;
+                //Quaternion q = new Quaternion(Camera.main.transform.right.x, Camera.main.transform.right.y, Camera.main.transform.right.z, 0);
+                //transform.Rotate(q.eulerAngles);
+                transform.RotateAround(Camera.main.transform.right, -m_FollowAngles.x/60); //TODO deprecated
+                
+                  
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Patient/ModelZoomer.cs
@@ -0,0 +1,59 @@
+﻿// Model Zoomer 
+//
+
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    public class ModelZoomer : MonoBehaviour
+    {
+
+
+        public float zoomingSpeed = 1;
+        public float maxZoom = 2f;
+        public float minZoom = 0.2f;
+
+
+        private float m_TargetZoom;
+        private float m_OriginalZoom;
+       
+
+        private void Start()
+        {
+            m_OriginalZoom = transform.localScale.x;
+        }
+
+
+        private void Update()
+        {
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+
+                //TODO make movement smooth
+
+                float inputScroll = Input.GetAxis("Mouse ScrollWheel");
+
+                m_OriginalZoom = transform.localScale.x;
+
+                m_TargetZoom = m_OriginalZoom + inputScroll/ (1/zoomingSpeed);
+
+                if (m_TargetZoom > maxZoom)
+                {
+                    m_TargetZoom = maxZoom;
+                }
+                if (m_TargetZoom < minZoom)
+                {
+                    m_TargetZoom = minZoom;
+                }
+
+                transform.localScale = new Vector3(m_TargetZoom, m_TargetZoom, m_TargetZoom);
+
+             
+            }
+        }
+    }
+}
+
+
