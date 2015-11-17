466fb6de9ed3d3f37de8c5cc139fa9f0beb01480
Matthias Huber <Matthias Huber>
2015-11-17 19:24:48 +0100

    Added StartScript

---
+++ Assets/Scripts/RecenterVR.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecenterVR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.VR.InputTracking.Recenter();
+        }
+    }
+}
