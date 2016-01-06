c58612e19cc56828401f03cd0f2a75e1da0c9815
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-01-06 16:39:57 +0100

    Sort of fixed widget offset when dragging.
    It's ugly. We should find a better way.

--- Assets/Scripts/UI/Control/DragableUI.cs
+++ Assets/Scripts/UI/Control/DragableUI.cs
@@ -12,6 +12,8 @@ public class DragableUI : MonoBehaviour {
 
     public GameObject mouse;
 
+	private Vector3 offset;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -34,8 +36,19 @@ public class DragableUI : MonoBehaviour {
                     if (Physics.Raycast(ray, out hit, Mathf.Infinity, onlyMousePlane))
                     {
                         //Vector3 offset = (mouse.transform.localPosition - Camera.main.transform.position).normalized * 10f;
-                        grandparent.localPosition = hit.point; // + offset;
-                        grandparent.transform.localRotation = Quaternion.FromToRotation(grandparent.transform.up, hit.normal) * grandparent.transform.localRotation;
+						grandparent.position = hit.point - offset;
+
+						// Vector from camera to position:
+						Vector3 diff = grandparent.position - Camera.main.transform.position;
+						diff.Normalize ();
+						grandparent.position = Camera.main.transform.position + diff * hit.distance;
+
+						// Trace toward center of widget:
+						RaycastHit hitCenter;
+						Ray rayCenter = new Ray(Camera.main.transform.position, grandparent.position - Camera.main.transform.position );
+						if (Physics.Raycast (rayCenter, out hitCenter, Mathf.Infinity, onlyMousePlane)) {
+							grandparent.transform.localRotation = Quaternion.FromToRotation (grandparent.transform.up, hitCenter.normal) * grandparent.transform.localRotation;
+						}
 
                     }
                 }
@@ -47,10 +60,20 @@ public class DragableUI : MonoBehaviour {
         }
     }
 
-    // Update is called once per frame
     public void MoveElement()
     {
-        dragUI = true;
+		if (dragUI == false) {
+			offset = new Vector3 (0, 0, 0);
+			RaycastHit hit;
+			Ray ray = new Ray (Camera.main.transform.position, mouse.transform.localPosition - Camera.main.transform.position);
+			LayerMask onlyMousePlane = 1 << 8; // hit only the mouse plane layer
+
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity, onlyMousePlane)) {
+				Transform grandparent = transform.parent.transform.parent;
+				offset = hit.point - grandparent.position;
+			}
+			dragUI = true;
+		}
     }
 
 }
