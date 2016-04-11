6b0d2af245302397d06bbc922b98730f4a70db64
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-04-12 00:38:31 +0200

    DicomViewer now also updates when created.
    This means if the patient was loaded beforehand, then the viewer will
    still set up the correct image.

--- Assets/Scripts/Tools/DicomWidget/DicomDisplay.cs
+++ Assets/Scripts/Tools/DicomWidget/DicomDisplay.cs
@@ -8,22 +8,32 @@ public class DicomDisplay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Transform tf = transform.Find ("Canvas/RawImage");
-		mDicomImage = tf.gameObject.GetComponent<RawImage>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	void Awake()
 	{
+		Transform tf = transform.Find ("Canvas/DicomImage");
+		mDicomImage = tf.gameObject.GetComponent<RawImage>();
 	}
 
 	void OnEnable()
 	{
 		DicomCache.startListening ("NewDicomLoaded", eventDisplayCurrentDicom );
+		DICOM dicom = DicomCache.getCurrentDicom();
+		if( dicom != null )
+		{
+			/*DicomRenderer.material.mainTexture = dicom.getTexture ();*/
+
+			mDicomImage.material.mainTexture = dicom.getTexture();
+			mDicomImage.material.SetFloat ("globalMaximum", (float)dicom.getMaximum ());
+			mDicomImage.material.SetFloat ("globalMinimum", (float)dicom.getMinimum ());
+			mDicomImage.material.SetFloat ("range", (float)(dicom.getMaximum () - dicom.getMinimum ()));
+		}
+
 	}
 
 	void OnDisable()
@@ -43,8 +53,6 @@ public class DicomDisplay : MonoBehaviour {
 			mDicomImage.material.SetFloat ("globalMinimum", (float)dicom.getMinimum ());
 			mDicomImage.material.SetFloat ("range", (float)(dicom.getMaximum () - dicom.getMinimum ()));
 		}
-
-
 	}
 
 	private RawImage mDicomImage;
