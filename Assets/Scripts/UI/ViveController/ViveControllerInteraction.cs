21817e11fb9d4c1a5c2566a0bce60de0cd5a1b09
Huber <huber@SMBAD.ITEC.UNI-KARLSRUHE.DE>
2016-05-12 15:41:01 +0200

    Detect trigger up for vive controller

--- Assets/Scripts/UI/ViveController/ViveControllerInteraction.cs
+++ Assets/Scripts/UI/ViveController/ViveControllerInteraction.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class ViveControllerInteraction : MonoBehaviour {
+
+	private SteamVR_Controller.Device controller { get{ return SteamVR_Controller.Input ((int)trackedObj.index);}}
+	private SteamVR_TrackedObject trackedObj;
+
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
 	private Mouse3DMovement mMouse;
 
 	// Use this for initialization
 	void Start () {
 		mMouse = GameObject.Find ("Mouse3D").GetComponent<Mouse3DMovement> ();
+		trackedObj = this.GetComponent<SteamVR_TrackedObject> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Raycast (move 3D mouse)
 		RaycastHit hit;
 		Ray ray = new Ray(transform.position,transform.forward);
 		LayerMask onlyMousePlane = 1 << 8; // hit only the mouse plane layer
@@ -22,5 +31,17 @@ public class ViveControllerInteraction : MonoBehaviour {
 			// Remember my UV coordinates, because the MouseUIInteraction script will use them to handle UI input:
 			mMouse.setUVCoordinates(hit.textureCoord2);
 		}
+
+		//Detect trigger
+		if (controller != null) {
+			if (controller.GetPressDown (triggerButton)) {
+				Debug.Log ("Trigger pressed");
+
+			}
+
+		} else {
+			Debug.LogWarning ("Controller is null");
+		}
+
 	}
 }
