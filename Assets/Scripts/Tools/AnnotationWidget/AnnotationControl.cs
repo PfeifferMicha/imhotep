226993ebe6664595de4eaeb60a4c818dede470e9
Matthias <matthias.huber2@student.kit.edu>
2016-05-18 23:20:08 +0200

    Fixed bug in Annotation control

--- Assets/Scripts/Tools/AnnotationWidget/AnnotationControl.cs
+++ Assets/Scripts/Tools/AnnotationWidget/AnnotationControl.cs
@@ -58,7 +58,6 @@ public class AnnotationControl : MonoBehaviour {
     {
         // Unregister myself:
         PatientEventSystem.stopListening(PatientEventSystem.Event.PATIENT_Loaded, loadAnnotationFromFile);
-        saveAnnotationInFile();
         clearAllPressed();
     }
 
