ae29ac87e3e102cfc851b8620ec68da244878cf5
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-06-03 17:56:11 +0200

    Worked on room animations.

---
+++ Assets/Scripts/RoomLightControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomLightControl : MonoBehaviour {
+
+	public GameObject[] roomLights;
+	public GameObject sun;
+
+	void Start () {
+		PatientEventSystem.startListening (PatientEventSystem.Event.PATIENT_StartLoading, lowerShutters);
+		PatientEventSystem.startListening (PatientEventSystem.Event.PATIENT_Closed, raiseShutters);
+	
+	}
+
+	public void lowerShutters( object obj = null )
+	{
+		GetComponent<Animation> () ["LowerShutters"].speed = 1f;
+		GetComponent<Animation> () ["LowerShutters"].time = 0f;
+		GetComponent<Animation> ().Play ("LowerShutters");
+	}
+	public void raiseShutters( object obj = null )
+	{
+		GetComponent<Animation> () ["LowerShutters"].speed = -1f;
+		GetComponent<Animation> ()["LowerShutters"].time = GetComponent<Animation> ()["LowerShutters"].length;
+		GetComponent<Animation> ().Play ("LowerShutters");
+		//RenderSettings.ambientIntensity = 1.5f;
+	}
+}
