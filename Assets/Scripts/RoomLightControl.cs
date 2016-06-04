be512bead0085ed8a54be22b626d5ccebb9948df
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-06-04 23:38:57 +0200

    Worked on scenes.

--- Assets/Scripts/RoomLightControl.cs
+++ Assets/Scripts/RoomLightControl.cs
@@ -6,23 +6,21 @@ public class RoomLightControl : MonoBehaviour {
 	public GameObject[] roomLights;
 	public GameObject sun;
 
+	private Animator animator;
+
 	void Start () {
 		PatientEventSystem.startListening (PatientEventSystem.Event.PATIENT_StartLoading, lowerShutters);
 		PatientEventSystem.startListening (PatientEventSystem.Event.PATIENT_Closed, raiseShutters);
-	
+
+		animator = GetComponent<Animator>();
 	}
 
 	public void lowerShutters( object obj = null )
 	{
-		GetComponent<Animation> () ["LowerShutters"].speed = 1f;
-		GetComponent<Animation> () ["LowerShutters"].time = 0f;
-		GetComponent<Animation> ().Play ("LowerShutters");
+		animator.SetTrigger ("Shut");
 	}
 	public void raiseShutters( object obj = null )
 	{
-		GetComponent<Animation> () ["LowerShutters"].speed = -1f;
-		GetComponent<Animation> ()["LowerShutters"].time = GetComponent<Animation> ()["LowerShutters"].length;
-		GetComponent<Animation> ().Play ("LowerShutters");
-		//RenderSettings.ambientIntensity = 1.5f;
+		animator.SetTrigger ("Open");
 	}
 }
