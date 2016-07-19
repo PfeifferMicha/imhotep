0e38f9ce1b666968bfe7ae5e68731532381e08db
Matthias <matthias.huber2@student.kit.edu>
2016-07-19 22:42:18 +0200

    Added Exit script (escape button quits the application)

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
