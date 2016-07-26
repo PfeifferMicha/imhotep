bfa0d9b03e582f20877ab7f72c1bf485dd949057
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-07-26 14:34:37 +0200

    Added tabs to PatientBriefing.

--- Assets/Scripts/Patient/Patient.cs
+++ Assets/Scripts/Patient/Patient.cs
@@ -13,11 +13,13 @@ public class Patient : PatientMeta
 	static private Patient loadedPatient = null;
 
 	private List<AdditionalInformation> additionalInformation = new List<AdditionalInformation> ();
+	private List<string> additionalInformationTabs = new List<string> ();
 
 	public class AdditionalInformation
 	{
 		public string name { get; set; }
 		public string content { get; set; }
+		public string tabName { get; set; }
 	}
 
 	public Patient( PatientMeta meta ) : base(meta)
@@ -35,7 +37,20 @@ public class Patient : PatientMeta
 		{
 			JsonData infoArray = data ["additional information"];
 
-			//foreach (JsonData elem in infoArray)
+			// Set up default tab:
+			additionalInformationTabs.Add ("General");
+			string c = "";
+			c += bold ("Patient Name: ") + name + "\n";
+			c += bold ("Date of Birth: ") + birthDate + "\n";
+			c += bold ("Date of Operation: ") + operationDate + "\n";
+			AdditionalInformation info = new AdditionalInformation {
+				name = "Patient Information",
+				content = c,
+				tabName = "General"
+			};
+			additionalInformation.Add (info);
+
+			// Find other information in files (which are given in meta.json) and add them to the tabs:
 			for (int i = 0; i < infoArray.Count; i++) {
 				JsonData entry = infoArray [i];
 				if (entry.Keys.Contains ("Name") && entry.Keys.Contains ("File")) {
@@ -43,13 +58,21 @@ public class Patient : PatientMeta
 					if( System.IO.File.Exists( base.path + "/" + entry ["File"] ) )
 					{
 						string content = System.IO.File.ReadAllText ( base.path + "/" + entry ["File"]);
-						string name = entry ["Name"].ToString();
+						string title = entry ["Name"].ToString();
+						string tabName = "General";
+						if (entry.Keys.Contains ("Tab")) {
+							tabName = entry ["Tab"].ToString();
+						}
 						if (name.Length > 0 && content.Length > 0) {
-							AdditionalInformation info = new AdditionalInformation {
-								name = name,
-								content = content
+							info = new AdditionalInformation {
+								name = title,
+								content = content,
+								tabName = tabName
 							};
 							additionalInformation.Add (info);
+							if (!additionalInformationTabs.Contains (tabName)) {
+								additionalInformationTabs.Add (tabName);
+							}
 						}
 					}
 				}
@@ -64,13 +87,25 @@ public class Patient : PatientMeta
 		return loadedPatient;
 	}
 
-	public string getAdditionalInfo()
+	public List<string> getAdditionalInfoTabs()
+	{
+		return additionalInformationTabs;
+	}
+	public string getAdditionalInfo( string tabName )
 	{
 		string result = "";
 		foreach (AdditionalInformation info in additionalInformation) {
-			result += "<b>" + info.name + "</b>" + "\n\n";
-			result += info.content + "\n\n";
+			// Add all info for this tab to the string:
+			if (info.tabName == tabName) {
+				result += "<b>" + info.name + "</b>" + "\n\n";
+				result += info.content + "\n\n";
+			}
 		}
 		return result;
+	}
+
+	private string bold( string input )
+	{
+		return "<b>" + input + "</b>";
 	}
 }
--- Assets/Scripts/Tools/PatientBriefing/PatientBriefing.cs
+++ Assets/Scripts/Tools/PatientBriefing/PatientBriefing.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 public class PatientBriefing : MonoBehaviour {
 
 	public GameObject textObj;
+	public GameObject tabButton;
 	private Text text;
 
 	// Use this for initialization
 	void OnEnable () {
+		tabButton.SetActive (false);
+
 		text = textObj.GetComponent<Text> ();
 		string msg = bold( "Patient Information" );
 		msg += "\n\nNo patient loaded.";
@@ -18,41 +23,48 @@ public class PatientBriefing : MonoBehaviour {
 		PatientEventSystem.startListening( PatientEventSystem.Event.PATIENT_Closed, eventPatientClosed );
 
 		Patient loadedPatient = Patient.getLoadedPatient ();
+		Debug.Log ("loaded patient:" + loadedPatient);
 		if (loadedPatient != null) {
-			setPatientText (loadedPatient);
+			eventNewPatientLoaded (loadedPatient);
 		} else {
 			eventPatientClosed ();
 		}
-
 	}
 
 	void eventNewPatientLoaded( object obj )
 	{
-		Patient p = obj as Patient;
-		if (p != null) {
-			setPatientText (p);
+		Patient patient = obj as Patient;
+		if (patient != null) {
+			
+			// Create Tabs according to the loaded tab names:
+			List<string> tabNames = patient.getAdditionalInfoTabs();
+			for (int i = 0; i < tabNames.Count; i++) {
+				GameObject newButton = GameObject.Instantiate (tabButton);
+				newButton.SetActive (true);
+				newButton.name = tabNames [i];
+				newButton.transform.FindChild ("Text").GetComponent<Text> ().text = tabNames [i];
+				newButton.transform.SetParent (tabButton.transform.parent, false);
+
+				string capturedTabName = tabNames [i];	// might not be necessary to capture, but just in case the list changes?
+				Button b = newButton.GetComponent<Button>();
+				b.onClick.AddListener(() => setPatientText( capturedTabName ));
+			}
+
+			setPatientText ("General");
 		}
 	}
 
-	private void setPatientText( Patient p ) {
-		string msg = bold( "Patient Information" );
-		msg += "\n\n";
+	private void setPatientText( string tabName ) {
 
-		msg += bold ("Patient Name: ") + p.name + "\n";
-		msg += bold ("Date of Birth: ") + p.birthDate + "\n";
-		msg += bold ("Date of Operation: ") + p.operationDate + "\n";
+		Patient loadedPatient = Patient.getLoadedPatient ();
+		if (loadedPatient != null) {
+			Debug.Log ("Loading tab: " + tabName);
+		}
 
-		msg += "\n";
-
-		msg += p.getAdditionalInfo() + "\n";
-
-		text.text = msg;
+		text.text = loadedPatient.getAdditionalInfo (tabName);
 	}
 
-	private string bold( string input )
-	{
-		return "<b>" + input + "</b>";
-	}
+
 
 	void eventPatientClosed( object obj = null )
 	{
@@ -62,5 +74,10 @@ public class PatientBriefing : MonoBehaviour {
 		msg += bold ("Patient Name: ") + "No patient loaded.";
 
 		text.text = msg;
+	}
+
+	private string bold( string input )
+	{
+		return "<b>" + input + "</b>";
 	}
 }
