ad10403b8e3c18936c463df43b426bc9bc133f28
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-07-27 09:33:10 +0200

    Started work on ViewControl tool.

---
+++ Assets/Scripts/Tools/ViewControl/ViewControl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewControl : MonoBehaviour {
+
+	private GameObject mainPane;
+	private GameObject editPane;
+	// Use this for initialization
+	void Start () {
+		mainPane = transform.FindChild ("Canvas/MainPane").gameObject;
+		editPane = transform.FindChild ("Canvas/EditPane").gameObject;
+
+		showMainPane ();
+	}
+
+	public void showMainPane()
+	{
+		editPane.SetActive (false);
+		mainPane.SetActive (true);
+	}
+	public void showEditPane()
+	{
+		mainPane.SetActive (false);
+		editPane.SetActive (true);
+	}
+
+	public void saveNewView()
+	{
+	}
+}
