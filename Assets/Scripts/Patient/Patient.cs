ba77802193a7dc0c23d204c4794e2581b2f551d6
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-07-27 14:24:03 +0200

    Can now create new views and toggle between them.

--- Assets/Scripts/Patient/Patient.cs
+++ Assets/Scripts/Patient/Patient.cs
@@ -14,12 +14,20 @@ public class Patient : PatientMeta
 
 	private List<AdditionalInformation> additionalInformation = new List<AdditionalInformation> ();
 	private List<string> additionalInformationTabs = new List<string> ();
+	private List<View> mViews = new List<View> ();
 
 	public class AdditionalInformation
 	{
 		public string name { get; set; }
 		public string content { get; set; }
 		public string tabName { get; set; }
+	}
+
+	public class View
+	{
+		public string name { get; set; }
+		public Quaternion orientation;
+		public Vector3 scale;
 	}
 
 	public Patient( PatientMeta meta ) : base(meta)
@@ -107,5 +115,26 @@ public class Patient : PatientMeta
 	private string bold( string input )
 	{
 		return "<b>" + input + "</b>";
+	}
+
+
+	// Views:
+	public List<View> getViews()
+	{
+		return mViews;
+	}
+	public int insertView( View newView, int index )
+	{
+		index = Mathf.Min (index, mViews.Count);
+		mViews.Insert (index, newView);
+		return index;
+	}
+	public View getView( int index )
+	{
+		if ( index >= 0 && mViews.Count > index) {
+			return mViews [index];
+		} else {
+			return null;
+		}
 	}
 }
--- Assets/Scripts/Tools/ViewControl/ViewControl.cs
+++ Assets/Scripts/Tools/ViewControl/ViewControl.cs
@@ -1,16 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class ViewControl : MonoBehaviour {
 
 	private GameObject mainPane;
 	private GameObject editPane;
+	public GameObject viewNameInputField;
+	public GameObject meshViewerRotationNode;
+	public GameObject meshViewerScaleNode;
+
+	private MeshLoader mMeshLoader;
+	public Button saveButton, editButton;
+	public Text viewNameText;
+	public Button buttoPrev, buttonNext;
+
+	private int currentViewIndex = 0;
+
 	// Use this for initialization
 	void Start () {
 		mainPane = transform.FindChild ("Canvas/MainPane").gameObject;
 		editPane = transform.FindChild ("Canvas/EditPane").gameObject;
 
+
+		mMeshLoader = GameObject.Find("GlobalScript").GetComponent<MeshLoader>();
+
+		patientClosed ();
 		showMainPane ();
+
+		Patient p = Patient.getLoadedPatient ();
+		if (p != null) {
+			meshLoaded ();
+		}
+	}
+
+	void OnEnable()
+	{
+		// Register event callbacks for MESH events:
+		PatientEventSystem.startListening(PatientEventSystem.Event.MESH_LoadedAll, meshLoaded);
+		PatientEventSystem.startListening(PatientEventSystem.Event.PATIENT_Closed, patientClosed); 
+	}
+
+	void OnDisable()
+	{
+		// Unregister myself - no longer receives events (until the next OnEnable() call):
+		PatientEventSystem.stopListening(PatientEventSystem.Event.MESH_LoadedAll, meshLoaded);
+		PatientEventSystem.stopListening(PatientEventSystem.Event.PATIENT_Closed, patientClosed);
+	}
+
+	public void meshLoaded( object obj = null )
+	{
+		Patient p = Patient.getLoadedPatient ();
+		List<Patient.View> views = p.getViews ();
+		if (views.Count > 0) {
+		} else {
+			viewNameText.text = "No views configured.";
+		}
+
+		saveButton.interactable = true;
+		editButton.interactable = true;
+
+		currentViewIndex = 0;
+		setView (currentViewIndex);
+	}
+	public void patientClosed( object obj = null )
+	{
+		saveButton.interactable = false;
+		editButton.interactable = false;
+		viewNameText.text = "No patient loaded.";
 	}
 
 	public void showMainPane()
@@ -23,8 +81,52 @@ public class ViewControl : MonoBehaviour {
 		mainPane.SetActive (false);
 		editPane.SetActive (true);
 	}
+	public void nextView()
+	{
+		setView (currentViewIndex + 1);
+	}
+	public void prevView()
+	{
+		setView (currentViewIndex - 1);
+	}
 
 	public void saveNewView()
 	{
+		Patient p = Patient.getLoadedPatient ();
+		if (p != null) {
+			string t = viewNameInputField.GetComponent<InputField> ().text;
+			if (t.Length > 0) {
+				if (mMeshLoader.MeshGameObjectContainers.Count != 0) {
+					//createContent();
+				}
+				Patient.View newView = new Patient.View ();
+				newView.name = t;
+				newView.orientation = meshViewerRotationNode.transform.localRotation;
+				newView.scale = meshViewerScaleNode.transform.localScale;
+
+				currentViewIndex = p.insertView ( newView, currentViewIndex + 1 );
+				setView (currentViewIndex);
+
+				showMainPane ();
+			}
+		}
+	}
+
+	void setView( int index )
+	{
+		Patient p = Patient.getLoadedPatient ();
+		if (p != null) {
+			Patient.View view = p.getView (index);
+			if (view != null) {
+				viewNameText.text = (index + 1).ToString();
+				viewNameText.text += ": ";
+				viewNameText.text += view.name;
+
+				meshViewerRotationNode.transform.localRotation = view.orientation;
+				meshViewerScaleNode.transform.localScale = view.scale;
+
+				currentViewIndex = index;
+			}
+		}
 	}
 }
