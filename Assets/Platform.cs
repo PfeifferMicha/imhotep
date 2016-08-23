efb93635c1c37b66f6d7d0346d96967303f90b10
pfeiffer <ueczz@student.kit.edu>
2016-08-23 12:51:23 +0200

    Rounded UI Plane is now auto-generated.

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Platform : MonoBehaviour {
 
@@ -14,8 +15,25 @@ public class Platform : MonoBehaviour {
 
 	public GameObject camera;
 
+	public Material UiMeshMaterial;
+
+	GameObject UIMesh = null;
+
 	float initialBaseWidth;
 	float initialBaseDepth;
+
+	[Tooltip("Radius of the rounded mesh")]
+	public float UIMeshRoundedRadius = 1.2f;
+	[Tooltip("Opening angle in degrees")]
+	public float UIMeshRoundedAngle = 160f;
+	[Tooltip("Distance from platform to bottom of the UI")]
+	public float UIMeshRoundedBottom = 0.3f;
+	[Tooltip("Height of the UI")]
+	public float UIMeshRoundedHeight = 2.0f;
+	[Tooltip("Distance from platform to bottom of the UI")]
+	public float UIMeshRectangularBottom = 0.5f;
+	[Tooltip("Height of the UI")]
+	public float UIMeshRectangularHeight = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,6 +45,8 @@ public class Platform : MonoBehaviour {
 
 		setRectangular ();
 		setDimensions (2f, 1.5f);
+
+		setRounded ();
 	}
 
 	/*! Set the size of the rectangular platform
@@ -86,6 +106,7 @@ public class Platform : MonoBehaviour {
 		chair.SetActive (true);
 
 		resetDimensions ();
+		generateUIMeshRounded ();
 	}
 
 	//! Enable the rectangular platform:
@@ -101,5 +122,78 @@ public class Platform : MonoBehaviour {
 		// Deactivate the rounded platform:
 		rounded.SetActive (false);
 		chair.SetActive (false);
+	}
+
+	/*! Generate a UI screen mesh for the rectangular platform. */
+	void generateUIMeshRectangular( float width, float depth )
+	{
+		removeUIMesh ();
+	}
+
+	/*! Generate a UI screen mesh for the rounded platform. */
+	void generateUIMeshRounded()
+	{
+		removeUIMesh ();
+
+		// First, initialize lists:
+		List<Vector3> newVertices = new List<Vector3> ();
+		List<Vector2> newUV = new List<Vector2> ();
+		List<int> newTriangles = new List<int> ();
+
+		// Fill the lists with vertices and triangles:
+		int numSegments = 20;
+		float fullAngle = UIMeshRoundedAngle * Mathf.PI / 180f;
+		for (int i = 0; i <= numSegments; i++) {
+			float currentAmount = (float)i / (float)numSegments;
+			float angle = fullAngle * currentAmount - fullAngle * 0.5f;
+			float x = UIMeshRoundedRadius * Mathf.Sin (angle);
+			float y = UIMeshRoundedRadius * Mathf.Cos (angle);
+			newVertices.Add (new Vector3 (x, 0, y));
+			newUV.Add (new Vector2 (currentAmount, 0));
+			newVertices.Add (new Vector3 (x, UIMeshRoundedHeight, y));
+			newUV.Add (new Vector2 (currentAmount, 1));
+		}
+		for (int i = 0; i <= numSegments - 1; i++) {
+			newTriangles.Add (i * 2 + 0);
+			newTriangles.Add (i * 2 + 1);
+			newTriangles.Add (i * 2 + 2);
+			newTriangles.Add (i * 2 + 1);
+			newTriangles.Add (i * 2 + 3);
+			newTriangles.Add (i * 2 + 2);
+		}
+
+		// Generate a mesh from the lists:
+		Mesh mesh = new Mesh();
+		mesh.name = "UIMesh";
+		mesh.vertices = newVertices.ToArray();
+		mesh.uv = newUV.ToArray();
+		mesh.triangles = newTriangles.ToArray();
+
+
+		// Generate a new game object:
+		GameObject go = new GameObject("UIMesh");
+		go.transform.SetParent( transform, false );
+		go.layer = LayerMask.NameToLayer ("MousePlane");
+		go.AddComponent<MeshFilter> ();
+		go.AddComponent<MeshCollider> ();
+		go.GetComponent<MeshFilter>().mesh = mesh;
+		go.GetComponent<MeshCollider> ().sharedMesh = mesh;
+
+		// Set up rendering:
+		MeshRenderer renderer = go.AddComponent<MeshRenderer> ();
+		renderer.material = UiMeshMaterial;
+
+		// Move forward until it reaches the edge of the platform:
+		float yPos = rounded.GetComponent<MeshRenderer>().bounds.size.z - UIMeshRoundedRadius;
+		go.transform.localPosition = new Vector3 (0f, UIMeshRoundedBottom, yPos);
+	}
+
+	/*! Remove UI Mesh, if present. */
+	void removeUIMesh()
+	{
+		if( UIMesh != null )
+		{
+			Destroy( UIMesh );
+		}
 	}
 }
