25638407cb13bb3d42f5089b019d8b48972cec43
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-10-02 14:35:20 +0200

    Added Controller model to each ToolStand.

---
+++ Assets/Scripts/ToolChoise.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ToolChoise : MonoBehaviour, IPointerEnterHandler {
+
+	public string toolName = "Default Tool Name";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnPointerEnter( PointerEventData eventData)
+	{
+		Debug.Log ("Entered: " + toolName);
+	}
+}
--- Assets/Scripts/ToolControl.cs
+++ Assets/Scripts/ToolControl.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 
 public class ToolControl : MonoBehaviour {
 
-	public GameObject ToolStand;
+	public GameObject ToolStandPrefab;
+	public GameObject ControllerPrefab;
 
 	List<GameObject> toolStands = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-		ToolStand.SetActive (false);
+		ToolStandPrefab.SetActive (false);
 
 		// Register event callbacks for all Patient events:
 		PatientEventSystem.startListening( PatientEventSystem.Event.PATIENT_Loaded, patientLoaded );
@@ -37,10 +38,21 @@ public class ToolControl : MonoBehaviour {
 		foreach (string s in availableTools) {
 			Debug.Log ("Generating tool stand for: " + s);
 			GameObject go = platform.toolStandPosition (i, (uint)availableTools.Count);
-			GameObject newToolStand = Object.Instantiate( ToolStand, Vector3.zero, Quaternion.identity) as GameObject;
+			GameObject newToolStand = Object.Instantiate( ToolStandPrefab, Vector3.zero, Quaternion.identity) as GameObject;
 			newToolStand.name = "ToolStand (" + s + ")";
 			newToolStand.transform.SetParent (go.transform, false);
 			StartCoroutine (activateToolStand (newToolStand, Random.value*0.25f + 0.3f*Mathf.Abs(availableTools.Count*0.5f - i)));
+
+			GameObject controllerChoise = Object.Instantiate (ControllerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+			controllerChoise.transform.localRotation = Quaternion.Euler (new Vector3 (0f, 270f, 270f));
+			controllerChoise.transform.localPosition = new Vector3 ( 0.2f, 0f, 0.13f );
+			ToolChoise tc = controllerChoise.GetComponent<ToolChoise> ();
+			tc.toolName = s;
+			Transform tableBone = newToolStand.transform.Find ("ToolStandArmature/BoneArm/BoneRotate/BoneSlide");
+			Debug.Log ("Found: " + tableBone);
+			controllerChoise.transform.SetParent( tableBone, false );
+			controllerChoise.SetActive (true);
+
 			i ++;
 		}
 	}
