239aff3b91087abc0152ce2e4b0e98fb8fdd6ce1
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-10-12 21:17:30 +0200

    Updated render script (can now use wildcards).

--- Raw/Images/UI/render.sh
+++ Raw/Images/UI/render.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-# dieses script rendert alle svg-dateien als *.png
+# Renders all *.svg files as *.png.
 
 resolution=150;
 
 shopt -s nullglob
+containsElement () {
+	local e
+	for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 1; done
+	return 0
+}
+
+echo -----------------------------------------------------
+echo Saving images:
+echo -----------------------------------------------------
+
 for infile in *.svg; do
-	echo $infile
-	outfile=../../../Assets/UI/Images/${infile%.*}.png
-	echo out $outfile
-	inkscape -f $infile -C -d $resolution --export-background=#000000 --export-background-opacity=0 -e $outfile
+	containsElement $infile $@ 
+	if [[ $# == 0 || $? == 1 ]]; then
+		outfile=../../../Assets/UI/Images/${infile%.*}.png
+		inkscape -f $infile -C -d $resolution --export-background=#000000 --export-background-opacity=0 -e $outfile
+	fi
 done
 
-mkdir ../../../Assets/UI/Images/Icons -p
-for infile in ./Icons/*.svg; do
-	echo $infile
-	outfile=../../../Assets/UI/Images/Icons/$(basename ${infile%.*}.png)
-	echo out $outfile
-	#outfile=../../../Assets/UI/Images/${infile%.*}.png
-	inkscape -f $infile -C -d $resolution --export-background=#000000 --export-background-opacity=0 -e $outfile
+mkdir -p ../../../Assets/UI/Images/Icons
+cd Icons
+for infile in *.svg; do
+	containsElement $infile $@ 
+	if [[ $# == 0 || $? == 1 ]]; then
+		outfile=../../../../Assets/UI/Images/Icons/${infile%.*}.png
+		echo $infile "=>" $outfile
+		inkscape -f $infile -C -d $resolution --export-background=#000000 --export-background-opacity=0 -e $outfile
+	fi
 done
+cd ..
