65e8f2c7eb9cbfc99657b6eeed7c7b19c1673107
Micha Pfeiffer <micha.pfeiffer@student.kit.edu>
2016-10-15 13:34:14 +0200

    Debug-Camera only rotates when mouse not over UI.

--- Assets/Scripts/Debug/RotateCamera.cs
+++ Assets/Scripts/Debug/RotateCamera.cs
@@ -12,11 +12,12 @@ public class RotateCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButton(2))
-        {
-            transform.localRotation = transform.localRotation * Quaternion.Euler(-Input.GetAxis("Mouse Y") * speed, Input.GetAxis("Mouse X") * speed, 0);
-			//transform.RotateAround( Vector3.zero, Vector3.up, -Input.GetAxis("Mouse X") * speed );
-			//transform.RotateAround( Vector3.zero, Vector3.right, -Input.GetAxis("Mouse Y") * speed );
-        }
+		if (!UI.Core.instance.pointerIsOverUIObject) {
+			if (Input.GetMouseButton (2)) {
+				transform.localRotation = transform.localRotation * Quaternion.Euler (-Input.GetAxis ("Mouse Y") * speed, Input.GetAxis ("Mouse X") * speed, 0);
+				//transform.RotateAround( Vector3.zero, Vector3.up, -Input.GetAxis("Mouse X") * speed );
+				//transform.RotateAround( Vector3.zero, Vector3.right, -Input.GetAxis("Mouse Y") * speed );
+			}
+		}
     }
 }
