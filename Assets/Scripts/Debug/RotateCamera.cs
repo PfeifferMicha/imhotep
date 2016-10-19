876c0016225c973a6750c67bd764566b57f8df55
Pfeiffer <pfeiffer@SMBAD.ITEC.UNI-KARLSRUHE.DE>
2016-10-19 10:45:08 +0200

    Camera Debug rotation now works by holding down CTRL.

--- Assets/Scripts/Debug/RotateCamera.cs
+++ Assets/Scripts/Debug/RotateCamera.cs
@@ -12,12 +12,10 @@ public class RotateCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!UI.Core.instance.pointerIsOverUIObject) {
-			if (Input.GetMouseButton (2)) {
+		if (( Input.GetMouseButton (0) || Input.GetMouseButton(1) || Input.GetMouseButton(2)) && Input.GetKey(KeyCode.LeftControl)) {
 				transform.localRotation = transform.localRotation * Quaternion.Euler (-Input.GetAxis ("Mouse Y") * speed, Input.GetAxis ("Mouse X") * speed, 0);
 				//transform.RotateAround( Vector3.zero, Vector3.up, -Input.GetAxis("Mouse X") * speed );
 				//transform.RotateAround( Vector3.zero, Vector3.right, -Input.GetAxis("Mouse Y") * speed );
-			}
 		}
     }
 }
