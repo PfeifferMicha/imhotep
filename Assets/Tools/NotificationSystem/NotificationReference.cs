18d5eece57221475bb0cf896566bdda02239b954
pfeiffer <ueczz@student.kit.edu>
2016-12-15 09:10:10 +0100

    Removed unused Start and Update functions from NotificationReference.

--- Assets/Tools/NotificationSystem/NotificationReference.cs
+++ Assets/Tools/NotificationSystem/NotificationReference.cs
@@ -3,16 +3,5 @@ using System.Collections;
 
 //! Used for notification game object to reference the acutal notifiaction
 public class NotificationReference : MonoBehaviour {
-
     public Notification notification;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
