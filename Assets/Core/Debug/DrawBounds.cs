39e51b335f789b38f3e483a235caa0ceaef22a39
pfeiffer <ueczz@student.kit.edu>
2016-12-19 12:36:47 +0100

    Added debug script to draw mesh bounding box.

---
+++ Assets/Core/Debug/DrawBounds.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawBounds : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool _bIsSelected = true;
+
+	void OnDrawGizmos()
+	{
+		if (_bIsSelected)
+			OnDrawGizmosSelected();
+	}
+
+
+	void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawSphere(transform.position, 0.1f);  //center sphere
+		if (transform.GetComponent<Renderer> () != null)
+			Gizmos.DrawWireCube (transform.GetComponent<Renderer> ().bounds.center, transform.GetComponent<Renderer> ().bounds.size);
+		else {
+			Bounds bounds = new Bounds ();
+			bool boundsInitialized = false;
+			Renderer[] renderers = GetComponentsInChildren<Renderer> ();
+			foreach (Renderer r in renderers) {
+				if (!boundsInitialized) {
+					bounds = r.bounds;
+					boundsInitialized = true;
+				} else {
+					bounds.Encapsulate (r.bounds);
+				}
+			}
+
+			Gizmos.DrawWireCube (bounds.center, bounds.size);
+		}
+	}
+}
