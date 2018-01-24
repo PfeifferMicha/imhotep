1a4fc06fe5834cd8145ee7629f45cc609021c29e
Arms <ARMSPHILL@med.tu-dresden.de>
2018-01-24 11:18:55 +0100

    Anzeige des Textes von der gedrückten Taste

---
+++ Assets/Tools/KeyboardControll/KeyboardControll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyboardControll : MonoBehaviour {
+	public Text input;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void clickEvent( string key )
+	{
+		input.text = input.text+key;
+	}
+
+	/*void getText(){
+		Component key = this.GetComponentsInChildren;
+
+		input.text = input.text + inputKey;
+	}**/
+}
