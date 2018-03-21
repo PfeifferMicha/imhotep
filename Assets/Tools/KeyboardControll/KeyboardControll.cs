b7f6a5cf7fb1e1af390cfabb506628573325e931
Arms <ARMSPHILL@med.tu-dresden.de>
2018-03-21 13:26:21 +0100

    Add possibility to switch between fields and add functionality of shift-key.

--- Assets/Tools/KeyboardControll/KeyboardControll.cs
+++ Assets/Tools/KeyboardControll/KeyboardControll.cs
@@ -17,13 +17,17 @@ public class KeyboardControll : MonoBehaviour{
 	private int caretPostionKeyboard;
 	//public SteamVR_TrackedObject tracked;
 	//public SteamVR_Controller.Device left;
-
+	//flag for big/little letters; false = big letter
+	private bool shift_flag;
+	//0:letterField is activated; 1:numberField is activated; 2:specialSignField is activated
+	private int activatedField_flag;
 	// Use this for initialization
 	void Start () {
 		if (annotationControl == null) {
 			annotationControl = GameObject.FindWithTag ("AnnotationControl");
 		}
 		caretPostionKeyboard = 0;
+		shift_flag = false;
 	}
 	//Save's the current caret Position
 	public void updateCaretPosition(){
@@ -34,12 +38,26 @@ public class KeyboardControll : MonoBehaviour{
 	void Update () {
 		
 	}
-
+		
+	public void setShift_Flag(){
+		if (shift_flag) {
+			shift_flag = false;
+		} else {
+			shift_flag = true;
+		}
+	}
 	//Enter's the text at the given caretPostion in the InputField of the Keyboard
-	public void enterTextEvent( string key )	{
+	public void enterTextEvent(string key )	{
 		keyboardInputField.text = keyboardInputField.text.Insert(caretPostionKeyboard,key);
 		selectedInputField.text = selectedInputField.text.Insert(caretPostionKeyboard,key);
 		caretPostionKeyboard++;
+	}
+
+	//Enter's a given letter
+	public void enterLetterEvent(string letter){
+		if (shift_flag) {
+			this.enterTextEvent (letter.ToLower ());
+		}
 	}
 
 	//Delete's the last Input-Symbol
@@ -51,6 +69,8 @@ public class KeyboardControll : MonoBehaviour{
 			keyboardInputField.text = keyboardInputField.text.Remove (keyboardInputField.text.Length - 1);
 		}
 	}
+
+
 	//Delete's the whole text
 	public void deleteText()
 	{
@@ -82,6 +102,38 @@ public class KeyboardControll : MonoBehaviour{
 	//Call's by activation
 	void OnEnable(){
 		this.setAnnotationControllerPosition ();
+	}
+
+
+	public void switchToField(int switchToField){
+		switch (activatedField_flag) {
+		case 0:
+		default:
+			GameObject.Find ("lettersField").SetActive (false);
+			break;
+		case 1:
+			GameObject.Find ("numbersField").SetActive (false);
+			break;
+		case 2:
+			GameObject.Find ("specialSignsField").SetActive (false);
+			break;
+		}
+		this.switcher (switchToField);
+	}
+	private void switcher(int switchToField){
+		switch (switchToField) {
+		case 0:
+		default:
+			GameObject.Find ("lettersField").SetActive (true);
+			break;
+		case 1:
+			GameObject.Find ("numbersField").SetActive (true);
+			break;
+		case 2:
+			GameObject.Find ("specialSignsField").SetActive (true);
+			break;
+		}
+		activatedField_flag = switchToField;
 	}
 
 	//Reset's the Annotation-Position
