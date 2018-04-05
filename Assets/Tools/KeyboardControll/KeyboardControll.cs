1810fcf13b4cfac341afa23d8e3181b7b1fba116
Arms <ARMSPHILL@med.tu-dresden.de>
2018-04-05 18:22:50 +0200

    Fix Missing PointerDown-Event with the Method:'buttonDeleteLastSymbolPressedDown'. And reorganize Code and add comments.

--- Assets/Tools/KeyboardControll/KeyboardControll.cs
+++ Assets/Tools/KeyboardControll/KeyboardControll.cs
@@ -38,6 +38,12 @@ public class KeyboardControll : MonoBehaviour{
 	//memorize normalColor of keyboardInputField for ReFocus
 	private Color normalColor;
 
+
+	//Call's by activation
+	void OnEnable(){
+		this.setAnnotationControllerPosition ();
+		this.endTextSelection = keyboardInputField.text.Length;
+	}
 	// Use this for initialization
 	void Start () {
 		if (annotationControl == null) {
@@ -52,10 +58,7 @@ public class KeyboardControll : MonoBehaviour{
 		buttonDeleteLastSymbolPressedDown = false;	
 		normalColor = keyboardInputField.colors.normalColor;
 	}
-	//Save's the current caret Position
-	public void updateCaretPosition(){
-		caretPostionKeyboard = keyboardInputField.caretPosition;
-	}
+
 	// Update is called once per frame
 	void Update () {
 		// Check if we clicked somewhere outside of the 
@@ -82,6 +85,10 @@ public class KeyboardControll : MonoBehaviour{
 			this.buttonDeleteLastSymbolPressedDown = false;
 		}
 	}
+	//Save's the current caret Position
+	public void updateCaretPosition(){
+		caretPostionKeyboard = keyboardInputField.caretPosition;
+	}
 	//Verifies, if a text within the Keyboard-Inpufield is selected
 	public void wasTextSelected(){
 		this.beginTextSelection = Mathf.Min (keyboardInputField.selectionFocusPosition, keyboardInputField.selectionAnchorPosition);
@@ -97,6 +104,11 @@ public class KeyboardControll : MonoBehaviour{
 			this.caretPostionKeyboard = 0;
 		}
 	}
+
+
+	//=========Method's for Entering Symbol in the Keyboard-Inputfield===========
+
+	//changes letter's from big to small or small to big
 	public void setShift_Flag(){
 		if (shift_flag) {
 			shift_flag = false;
@@ -125,34 +137,6 @@ public class KeyboardControll : MonoBehaviour{
 		this.reFocusKeyboardInputfield ();
 	}
 
-	//Set the focus back to the keyboard-Inputfield
-	private void reFocusKeyboardInputfield(){
-		EventSystem.current.SetSelectedGameObject (keyboardInputField.gameObject);
-		keyboardInputField.selectionFocusPosition = 0;
-		keyboardInputField.selectionAnchorPosition = 0;
-		beginTextSelection = 0;
-		endTextSelection = 0;
-		keyboardInputField.caretPosition = caretPostionKeyboard;
-		StartCoroutine (waitForFrame ());
-	}
-	public void changeNormalColorIntoHighlightedColorForRefocus(){
-		ColorBlock tempBlock = keyboardInputField.colors;
-		tempBlock.normalColor = keyboardInputField.colors.highlightedColor;
-		keyboardInputField.colors = tempBlock;
-		Debug.Log (keyboardInputField.colors.ToString ());
-	}
-	public void changeNormalColorBackAfterRefocus(){
-		ColorBlock tempBlock = keyboardInputField.colors;
-		tempBlock.normalColor = normalColor;
-		keyboardInputField.colors = tempBlock;
-	}
-	//used to deselect the text of keyboard-Inputfield
-	private IEnumerator waitForFrame(){
-		yield return 0;
-		keyboardInputField.MoveTextStart (false);
-		keyboardInputField.caretPosition = caretPostionKeyboard;
-	}
-
 	//Enter's a given letter
 	public void enterLetterEvent(string letter){
 		if (shift_flag) {
@@ -178,9 +162,18 @@ public class KeyboardControll : MonoBehaviour{
 		}
 		this.reFocusKeyboardInputfield ();
 	}
-	public void buttonDeleteLastSymbolPressingDown(){
-		this.buttonDeleteLastSymbolPressedDown = true;
+	//Make's linebreak in the keyboard-inputfield
+	public void lineBreak(){		
+		keyboardInputField.text += "\n";
+		if (selectedInputField != null) {
+			selectedInputField.text += "\n";
+		}
+		this.caretPostionKeyboard++;
+		this.deleteSelectedText ();	
+		this.wasTextSelected ();
+		this.reFocusKeyboardInputfield ();
 	}
+
 	//Delete's the whole text
 	public void deleteText()
 	{
@@ -216,22 +209,7 @@ public class KeyboardControll : MonoBehaviour{
 		this.setAnnotationControllerPositionBack ();
 	}
 
-	//Call's by activation
-	void OnEnable(){
-		this.setAnnotationControllerPosition ();
-		this.endTextSelection = keyboardInputField.text.Length;
-	}
-
-	public void lineBreak(){		
-		keyboardInputField.text += "\n";
-		if (selectedInputField != null) {
-			selectedInputField.text += "\n";
-		}
-		this.caretPostionKeyboard++;
-		this.deleteSelectedText ();	
-		this.wasTextSelected ();
-		this.reFocusKeyboardInputfield ();
-	}
+	//Switches between the different Fields (for numbers/letter/special signs) of the keyboard
 	public void switchToField(int switchToField){
 		switch (activatedField_flag) {
 		case 0:
@@ -263,7 +241,10 @@ public class KeyboardControll : MonoBehaviour{
 		}
 		this.activatedField_flag = switchToField;
 	}
-
+	//Helps to determ,if the button "DeleteLastSymbol" is pressed down for continously Deletion of the text
+	public void buttonDeleteLastSymbolPressingDown(){
+		this.buttonDeleteLastSymbolPressedDown = true;
+	}
 	//Reset's the Annotation-Position
 	private void setAnnotationControllerPosition(){
 		Rect sideScreenRect = this.gameObject.GetComponentInChildren<RectTransform> ().rect;
@@ -273,5 +254,35 @@ public class KeyboardControll : MonoBehaviour{
 	private void setAnnotationControllerPositionBack(){
 		Rect sideScreenRect = this.gameObject.GetComponentInChildren<RectTransform> ().rect;
 		annotationControl.transform.Translate (new Vector3 ((-sideScreenRect.width/1000)*2, 0));
+	}
+
+
+	//================Method's for Refocus to the Keyboard-InputField after a Button is clicked=========
+	//Set the focus back to the keyboard-Inputfield
+	private void reFocusKeyboardInputfield(){
+		EventSystem.current.SetSelectedGameObject (keyboardInputField.gameObject);
+		keyboardInputField.selectionFocusPosition = 0;
+		keyboardInputField.selectionAnchorPosition = 0;
+		beginTextSelection = 0;
+		endTextSelection = 0;
+		keyboardInputField.caretPosition = caretPostionKeyboard;
+		StartCoroutine (waitForFrame ());
+	}
+	public void changeNormalColorIntoHighlightedColorForRefocus(){
+		ColorBlock tempBlock = keyboardInputField.colors;
+		tempBlock.normalColor = keyboardInputField.colors.highlightedColor;
+		keyboardInputField.colors = tempBlock;
+		Debug.Log (keyboardInputField.colors.ToString ());
+	}
+	public void changeNormalColorBackAfterRefocus(){
+		ColorBlock tempBlock = keyboardInputField.colors;
+		tempBlock.normalColor = normalColor;
+		keyboardInputField.colors = tempBlock;
+	}
+	//used to deselect the text of keyboard-Inputfield
+	private IEnumerator waitForFrame(){
+		yield return 0;
+		keyboardInputField.MoveTextStart (false);
+		keyboardInputField.caretPosition = caretPostionKeyboard;
 	}
 }
