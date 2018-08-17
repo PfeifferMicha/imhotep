34f7b281df7c3b4e62b197105faac8957c4fbfdb
Arms <ARMSPHILL@med.tu-dresden.de>
2018-08-17 13:02:18 +0200

    Add comments for 'AutoCompleteControl' and fixed a bug for applieing the suggested word in the Keyboard-Inputfield.

--- Assets/Tools/KeyboardControl/AutoCompleteControl.cs
+++ Assets/Tools/KeyboardControl/AutoCompleteControl.cs
@@ -3,25 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+/*
+ * Responsible for inserting new Words in the Dictionary, but also for applying autocomplete-Suggestion selected by the user.
+ * It's uses an Interface (IEnteredText) to get the saved text by the user in order to insert new words.
+ * The Keyboard is the Caller.
+ */
 public class AutoCompleteControl : MonoBehaviour, IEnteredText{
 	//When insert a new word?
 	/* Wort: Ende/Anfangsmarker: "Leerzeichen"; ","; "."; "!"; "?"; ";" 
 	 * 
 	 */
+	//Seperation signs for marking words
 	private string[] seperator = {" ",",",".","!","?",";"};
+	//Inputfield of the keyboard
 	public InputField input;
+
+	//Contains the possible suggestions
 	public Text suggestionTop;
 	public Text suggestionMiddle;
 	public Text suggestionBottom;
 
 	//Dictionary for Autocomplete
 	DictEntryMultyWord autoCompleteDic = new DictEntryMultyWord ();
-	// Use this for initialization
+	// Use this for initialization 
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
+	//Searching the text for words in order to find likely matches
 	void Update () {
 		List<DictEntrySingleWord> tempLikelyWords = new List<DictEntrySingleWord>();
 		/*<DictEntrySingleWord> stringlist = autoCompleteDic.getSortedLikelyWordsAfterRate("");
@@ -40,8 +50,7 @@ public class AutoCompleteControl : MonoBehaviour, IEnteredText{
 		suggestionBottom.text = suggest [2];
 	}
 
-
-
+	//Interface method, called by the keyboard
 	public void enteredText(string text){
 		string[] words = this.getWordsFromInput (text);
 		if (words != null) {
@@ -55,18 +64,26 @@ public class AutoCompleteControl : MonoBehaviour, IEnteredText{
 		*/
 	}
 
+	//Applie's the suggestion-word in the inputfield of the keyboard, selected by the user
 	public void applySuggestion(){
 		GameObject selected = EventSystem.current.currentSelectedGameObject;
 		if (selected != null) {
+			//selected suggested-word
 			string suggestionText = selected.GetComponentInChildren<Text> ().text;
 			int maxlength = suggestionText.Length;
+			//whole length of Inputfield - text
 			int maxLengthInput = input.text.Length;
+			//Idea: go to the end of the text minus the length of the suggested-word
 			int startIndex = (maxLengthInput - maxlength) > 0 ? (maxLengthInput - maxlength) : 0; 
-			string textToSearch = this.input.text.Substring (startIndex);
+			string textToSearch =  this.input.text.Substring (startIndex).ToLower();
 			bool foundPartOfSuggestionText = false;
 			int length = maxlength;
+			/*Motivation: Have to findout how many letters of the suggested-word are already entered 
+			* in order to insert the suggested-word at the right position in the inputfield text
+			* Approach: Compare the end of the text with parts of the suggested-word; start with the whole word, reduce it every iteration
+			*/
 			while (!foundPartOfSuggestionText & length>=0){
-				string tempCompare = suggestionText.Substring (0,length);
+				string tempCompare = suggestionText.Substring (0,length).ToLower();
 				int index = textToSearch.IndexOf (tempCompare);
 				if (index != -1 & maxLengthInput>(maxLengthInput - maxlength + index)) {
 					foundPartOfSuggestionText = true;
@@ -77,8 +94,10 @@ public class AutoCompleteControl : MonoBehaviour, IEnteredText{
 				}
 			}
 		}
+		//Set the Focus back to the keyboard-Inputfield
 		this.reFocusKeyboardInputfield ();
 	}
+	//Extract all words from a given text
 	private string[] getWordsFromInput(string text){
 		if (text != null) {			
 			string[] words = text.Split (seperator, System.StringSplitOptions.RemoveEmptyEntries);
